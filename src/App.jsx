c6a1e4c819bfa5269d829b555ef5656d3476df2d
sushant_kumar <sushant.singh8899@gmail.com>
2024-02-16 00:00:15 +0530

    Added state and made functionality of buttons using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import {useState} from 'react';
 import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import {EXAMPLES} from './data.js'
 
 function App() {
+  const [selectedTopic, setSelectedTopic ] = useState('components');
+
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedButton);
+}
   return (
     <div>
       <Header />
@@ -26,12 +35,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,3 +1,11 @@
-export default function TabButton({children}){
-    return <li><button>{children}</button></li>;
+export default function TabButton({ children, onSelect }) {
+    /* function handleClick() {
+        console.log('Hello World');
+    }
+*/
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
 }
