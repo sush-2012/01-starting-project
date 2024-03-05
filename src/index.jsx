54c2abdc373a6f2e50090755f2e13b2a25cacc90
sushant_kumar <sushant.singh8899@gmail.com>
2024-03-05 22:50:47 +0530

    commit for githubpages

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 import App from "./App.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+//ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.render(<App />, entryPoint);
